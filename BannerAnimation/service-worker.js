c870d695ce2e69c9f84c215502acd8b77577306e
ToniNaumoski <tn2701@gmail.com>
2020-09-13 19:46:53 +0200

    new commit

--- BannerAnimation/service-worker.js
+++ BannerAnimation/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BannerAnimation/precache-manifest.5691685a8554ba662f2dca80bec4d60e.js"
+  "/precache-manifest.d0d8ca761021bf90e22a62ba63aa8fae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BannerAnimation/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
